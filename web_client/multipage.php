d3f57af87b9433a7f06f828043d52ea1e00da4d3
Carlos Silva <csaleman@gmail.com>
2013-04-07 13:12:45 -0400

    This version include a rudimentary web client based on mosquitto websocket.

---
+++ web_client/multipage.php
@@ -0,0 +1,86 @@
+
+<!DOCTYPE html> 
+<html> 
+<head> 
+	<title>Page Title</title> 
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1"> 
+
+	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.3.0/jquery.mobile-1.3.0.min.css" />
+	<script type="text/javascript" src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
+	<script type="text/javascript" src="http://code.jquery.com/mobile/1.3.0/jquery.mobile-1.3.0.min.js"></script>
+ 	<script type="text/javascript" src="http://www.modernizr.com/downloads/modernizr-latest.js"></script>
+	<script type="text/javascript" src="mosquitto-1.1.js"></script>
+	<script type="text/javascript" src="yavrha.js"></script>
+
+    <style>
+        #message {
+            display: none;
+            border-radius: 10px;
+            padding: 10px;
+            background-color: #ff0000;
+            color: #fff;
+        }
+    </style>
+    <script type="text/javascript">
+        
+
+    </script>
+</head> 
+
+<body> 
+
+<!-- Start of first page -->
+<div data-role="page" id="main">
+	
+<div data-role="header" data-position="inline">
+		<h1>Yavrha Home</h1>
+		<a href="#settings" data-icon="gear" class="ui-btn-right">&nbsp;</a>
+	</div>
+	<!-- /header -->
+
+	<div data-role="content" id="MainUlDiv">	
+		<ul data-role="listview" data-inset="true" id="MainUl">
+
+        </ul>		
+
+</div><!-- /content -->
+
+</div><!-- /page -->
+
+
+<!-- Start of second page -->
+<div data-role="page" id="settings">
+
+	<div data-role="header" data-position="inline">
+		<a href="#main">Back</a>
+		<h1>Settings</h1>
+	</div><!-- /header -->
+
+	<div data-role="content">	
+		 <p id="message"/>
+        <ul data-role="listview" data-inset="true">
+            <li data-role="list-divider">Mosquitto Websocket Server - ws://test.mosquitto.org/mqtt</li>
+            <li> <input type="text" id="MosquittoServer" name="MosquittoServer" placeholder="Enter mosquitto server"/>
+	    <li data-role="list-divider">Mosquitto Topic - topic/#</li>
+            <li> <input type="text" id="MosquittoTopic" name="MosquittoTopic" placeholder="Enter mosquitto topic"/>
+	    <li data-role="list-divider">Mosquitto Server User</li>
+            <li> <input type="text" id="MosquittoServerUser" name="MosquittoServerUser" placeholder="Enter mosquitto server User"/>
+	    <li data-role="list-divider">Mosquitto Server Password</li>
+            <li> <input type="text" id="MosquittoServerPassword" name="MosquittoServerPassword" placeholder="Enter mosquitto server Password"/>                 
+		
+	    <input type="button" id="addToStorage" value="Save"/>
+            </li>
+        </ul>		
+			
+	</div><!-- /content -->
+
+	
+</div><!-- /page -->
+</body>
+
+
+
+</body>
+</html>
+
---
+++ web_client/yavrha.js
@@ -0,0 +1,267 @@
+// Global Variables
+	var localStorageKey1 = "MosquittoServer";
+	var localStorageKey2 = "MosquittoTopic";
+ 	var localStorageKey3 = "MosquittoServerUser";
+	var localStorageKey4 = "MosquittoServerPassword";
+	var TOPIC = "";
+
+// Mosquitto Object
+	var t = new Mosquitto();
+
+// Nodes Object
+// {
+//	 "node1":{  "name":"2nd_temp",
+//				"type":"1",
+//				"address":"230",
+//				"number":"1",
+//				"msg":"{'msgid': 83, 'data1': 1, 'data0': 19, 'data3': 0, 'data2': 0}",
+//				"msgid":"83",
+//				"data0":"19",
+//				"data1":"1",
+//				"data2":"0",
+//				"data3":"0" }
+//	"node2": { etc .. .. 
+
+	var NodesObj = {};
+
+//********************************************************
+// 		This is the function that initiate jquery.
+//********************************************************
+
+        $('#main').live('pageinit', function() {
+//		Check if local storage is suported in the browser           
+			if (Modernizr.localstorage) {
+                showStoreValue();
+				MosquittoSetup(localStorage.getItem(localStorageKey1),localStorage.getItem(localStorageKey2));
+            }
+            else {
+                $('#message').text("Unfortunately your browser doesn't support local storage");
+                $('#addToStorage').attr('disabled', 'disabled');
+                $('#message').show();
+            }
+           
+	        $('#addToStorage').click(function(e) {
+//				Save values into localstorage                
+				localStorage.setItem(localStorageKey1, $('#MosquittoServer').val());
+				localStorage.setItem(localStorageKey2, $('#MosquittoTopic').val());
+				localStorage.setItem(localStorageKey3, $('#MosquittoServerUser').val());
+				localStorage.setItem(localStorageKey4, $('#MosquittoServerPassword').val());
+//				Show Refresh values in page.
+				showStoreValue();
+//				Prevent Default action. In this case submit form.
+                e.preventDefault();
+//				Reload page with new settings.
+				location.reload();
+
+            });
+        });
+
+//********************************************************
+// Function to get values from browser's local storage
+//********************************************************
+function showStoreValue() {
+// Mosquitto Server
+        	
+		$('#MosquittoServer').val(localStorage.getItem(localStorageKey1));
+       
+// Mosquitto Topic
+	var item2 = localStorage.getItem(localStorageKey2);
+        	
+		$('#MosquittoTopic').val(item2);
+
+// Load Topic in global variable TOPIC		
+		TOPIC = item2;           
+
+// Mosquitto Server User
+	var item3 = localStorage.getItem(localStorageKey3);
+        
+		$('#MosquittoServerUser').val(item3);
+
+// Mosquitto Server Password
+	var item4 = localStorage.getItem(localStorageKey4);
+        		
+		$('#MosquittoServerPassword').val(item4);
+}
+
+//********************************************************
+// This function configure the mosquitto instance with a server and topic.
+//********************************************************
+function MosquittoSetup(server,topic)
+{
+   if (server != null && topic != null) {
+    	t.connect(server,100000);
+    	t.subscribe(topic,0);
+// Set callback function
+		t.onmessage = StoreMsg;
+	}
+
+}
+//********************************************************
+// This function is a callback from the mosquitto instance
+//********************************************************
+function StoreMsg(Topic, Payload, qos, retain){
+
+//  alert(topic);
+	SubMsg = document.getElementById('SubMsg');
+	
+//  Split topic to get Node Number ej. "topic/node1/name"
+
+	TopicArray = Topic.split('/');
+	NodeNumber = TopicArray[1];
+	MsgName	   = TopicArray[2];
+	
+
+//	cannot add value in an object that doesn't exist yet.
+//	this create an emptly object for the node number if it doesn't exist
+
+	if (typeof NodesObj[NodeNumber]=='undefined'){
+		
+		NodesObj[NodeNumber] = {};
+		NodesObj[NodeNumber][MsgName] = Payload;
+	}
+	else {
+		
+//		Save the value "Payload" in the property "MsgName" for a given Node
+		NodesObj[NodeNumber][MsgName] = Payload;
+		
+	}
+
+//	If message is "Number" which is the last configuration message sent by the python client call the PaintScreen function.
+
+	if (MsgName == "number") {
+		
+		PaintScreen();
+	}
+	
+		UpdateValues();
+
+	 //DebugWindow.value = JSON.stringify(NodesObj);
+	 console.log(NodesObj);	
+
+}
+
+//********************************************************
+// This function create  the custom forms widgets
+//********************************************************
+function PaintScreen()
+{
+//		Remove all li in listview
+		$('ul#MainUl').children().remove('li');
+//		Print a debug text area       
+//		$('ul#MainUl').append('<li data-role="list-divider">Debug Window</li><li> <textarea rows="5" cols="25" id="DebugWindow"></textarea>');
+
+	for (var key in NodesObj) {
+
+		Node = NodesObj[key];
+		
+		if (Node["type"] > 127) {
+			
+			switch (parseInt(Node["type"])){
+				
+				case 128:
+					$('ul#MainUl').append('<li data-role="list-divider">Switch ' + Node["name"] + ' </li>');
+			 		$('ul#MainUl').append('<li><select name="node'+ Node["number"] + '" id="node'+ Node["number"] + '" data-role="slider"> \
+										<option value="0">Off</option> <option value="1">On \
+										</option> </select></li>');
+					$('#node'+ Node["number"] ).on( 'slidestop', function( event ) { PostMessage($(this).attr('id') ,$(this).attr('value')); });
+					break;
+				case 129:
+					$('ul#MainUl').append('<li data-role="list-divider">Dimmer ' + Node["name"] + ' </li>');
+			 		$('ul#MainUl').append('<li> <input type="text" id="Node1" name="Node1"/> </li>');
+					break;
+				case 130:
+					$('ul#MainUl').append('<li data-role="list-divider">Servo ' + Node["name"] + ' </li>');
+			 		$('ul#MainUl').append('<li> <input type="text" id="Node1" name="Node1"/> </li>');
+					break;
+					
+
+			}
+		} 
+	}
+		
+	for (var key in NodesObj) {
+		
+		Node = NodesObj[key];
+		
+		if (Node["type"] < 128) {
+			
+			switch (parseInt(Node["type"])){
+				
+				case 1:
+					$('ul#MainUl').append('<li data-role="list-divider">Temperature ' + Node["name"] + ' </li>');
+			 		$('ul#MainUl').append('<li id="node'+ Node["number"]+'" >00.0</li>');
+					break;
+				case 2:
+					$('ul#MainUl').append('<li data-role="list-divider">Status ' + Node["name"] + ' </li>');
+			 		$('ul#MainUl').append('<li> <input type="text" id="Node1" name="Node1"/> </li>');
+					break;
+				case 3:
+					$('ul#MainUl').append('<li data-role="list-divider">Power ' + Node["name"] + ' </li>');
+			 		$('ul#MainUl').append('<li> <input type="text" id="Node1" name="Node1"/> </li>');
+					break;
+					
+			}
+		}
+	}
+
+
+
+// 	This refresh the CSS in the list.
+//	This works in JQ Mobile 1.3+
+//	$('#MainUl').listview("refresh");
+// 	This works in all JQ Mobile Version 	 
+	$('#MainUl').listview("refresh").trigger("create");
+}
+
+
+//********************************************************
+// Function to update values
+//********************************************************
+function UpdateValues(){
+
+		for (var key in NodesObj) {
+						
+			Node = NodesObj[key];
+
+			switch (parseInt(Node["type"])){
+				
+				case 1:
+					var sign = (Node["data1"] = 1) ? "" : "-";
+					var tempC = parseInt(sign + Node["data0"] );
+					var tempF = Math.round((tempC*1.8)+32);
+					$('#'+key).text(tempC + "  \u00B0C ( " + tempF + " \u00B0F )"  );	
+					break;
+				case 2:
+					
+					break;
+				case 3:
+					
+					break;
+				case 128:
+					$('#'+key).val(Node["data0"]);
+					$('#'+key).slider("refresh");
+					//alert(key + ":" + Node["data0"]);
+					break;
+				case 129:
+					
+					break;
+				case 130:
+					
+					break;
+
+
+			}
+		}
+}
+
+  
+//********************************************************
+// Function to Post Messages back to MQTT server
+//********************************************************
+function PostMessage(Node, PostVal)
+{
+   postTopic = TOPIC.substring(0, TOPIC.length - 1) + Node + "/cmd";
+   t.publish(postTopic, PostVal,0,0);
+
+	//alert(postTopic);
+}
